419afb4d93ec6bbafe2d3ba0448c5dad6eb375ec
Alex <alex@hagl.de>
2023-02-20 16:15:14 +0100

    Added api key auth

--- app.py
+++ app.py
@@ -1,13 +1,27 @@
 # -*- coding: utf-8 -*-
-
-from flask import Flask, jsonify
+from functools import wraps
+from flask import Flask, jsonify, request, abort
 from discord.BotApiModel import BotApiModel
 import discord.manager as botMan
+from dotenv import load_dotenv
+import os
 
 app = Flask(__name__)
 
+load_dotenv()
+
+def require_appkey(view_function):
+    @wraps(view_function)
+    def decorated_function(*args, **kwargs):
+        if request.headers.get('x-api-key') and request.headers.get('x-api-key') == os.getenv('API_TOKEN'):
+            return view_function(*args, **kwargs)
+        else:
+            abort(401)
+    return decorated_function
+
 
 @app.route('/api/discordbots', methods=['GET'])
+@require_appkey
 def get_bots():
     botModels = []
     for bot in botMan.get_bots():
@@ -21,18 +35,21 @@ def get_bots():
 
 
 @app.route('/api/discordbots/killall', methods=['DELETE'])
+@require_appkey
 def killall_bots():
     botMan.killall()
     return "Ok", 200
 
 
 @app.route('/api/discordbots/kill/<int:id>', methods=['DELETE'])
+@require_appkey
 def kill_bot(id):
     botMan.kill(id)
     return "Ok", 200
 
 
 @app.route('/api/discordbots/run/<int:id>', methods=['POST'])
+@require_appkey
 def run_bot(id):
     botMan.start(id)
     return "Ok", 200
